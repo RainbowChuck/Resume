4823dc2ae008ecdc7b21f90bb87a09d56b3fdb86
goncharovmikhail <goncharov1752@gmail.com>
2025-06-08 23:56:45 +0300

    SearchHistory class updated

--- models.py
+++ models.py
@@ -1,12 +1,26 @@
-# models.py
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 from database import Base
 import datetime
 
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
+    username = Column(String, unique=True)
+    hashed_password = Column(String)
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
     role = Column(String, default="hr")
+
+    history = relationship("SearchHistory", back_populates="user")
+
+class SearchHistory(Base):
+    __tablename__ = "search_history"
+    id = Column(Integer, primary_key=True, index=True)
+    query = Column(String)
+    city = Column(String)
+    results = Column(Integer)
+    date = Column(DateTime, default=datetime.datetime.utcnow)
+
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="history")
+
