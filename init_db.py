01881097a0e7f7b41403d0afea6bb4ec7b80e0e0
goncharovmikhail <goncharov1752@gmail.com>
2025-06-09 11:03:28 +0300

    init_db update

--- init_db.py
+++ init_db.py
@@ -2,5 +2,10 @@
 from database import engine
 from models import Base
 
-Base.metadata.create_all(bind=engine)
-print("База данных создана.")
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+if __name__ == "__main__":
+    print("Creating database tables...")
+    init_db()
+    print("Database tables created successfully!")
